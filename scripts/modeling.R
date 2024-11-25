c92f245f96a22c778aa02a9b1d95e2515067b001
SameeckBhatia <canadianmg@outlook.com>
2024-11-25 18:39:09 -0500

    Exporting the model

--- scripts/modeling.R
+++ scripts/modeling.R
@@ -27,6 +27,7 @@ model_data <- cleaned_data |>
   select(property_type, zip_code, beds, baths, half_bath, sqft, days_on_market,
          property_age, price)
 
+# Setting seed for reproducibility
 set.seed(12)
 
 split <- initial_split(model_data, prop = 0.7)
@@ -41,15 +42,12 @@ model1 <- lm(data = training_data,
 y_pred <- predict(model1, newdata = testing_data)
 y_true <- testing_data$price
 
+# Evaluating model 1
 rmse1 <- RMSE(y_pred, y_true)
 mae1 <- MAE(y_pred, y_true)
 r2_1 <- R2_Score(y_pred, y_true)
 
 summary(model1)
-
-plot(fitted(model1), resid(model1))
-qqnorm(resid(model1));qqline(resid(model1))
-powerTransform(model1)
 
 # Fitting model 2
 model2 <- lm(data = training_data, 
@@ -58,6 +56,7 @@ model2 <- lm(data = training_data,
 y_pred <- predict(model2, newdata = testing_data)
 y_true <- testing_data$price
 
+# Evaluating model 2
 rmse2 <- RMSE(y_pred, y_true)
 mae2 <- MAE(y_pred, y_true)
 r2_2 <- R2_Score(y_pred, y_true)
@@ -66,6 +65,10 @@ summary(model2)
 
 anova(model1, model2)
 
+plot(fitted(model2), resid(model2))
+qqnorm(resid(model2));qqline(resid(model2))
+powerTransform(model2)
+
 # Fitting model 3
 model3 <- lm(data = training_data, 
              formula = price^(-1/4) ~ property_type + beds + sqft + property_age + sqft:beds)
@@ -73,11 +76,15 @@ model3 <- lm(data = training_data,
 y_pred <- predict(model3, newdata = testing_data)
 y_true <- testing_data$price^(-1/4)
 
+# Evaluating model 3
 rmse3 <- RMSE(y_pred, y_true)
 mae3 <- MAE(y_pred, y_true)
 r2_3 <- R2_Score(y_pred, y_true)
 
 summary(model3)
+
+plot(fitted(model3), resid(model3))
+qqnorm(resid(model3));qqline(resid(model3))
 
 # Summarizing metrics
 data.frame(
@@ -86,3 +93,6 @@ data.frame(
   model3 = c(rmse3, mae3, r2_3),
   row.names = c("RMSE", "MAE", "R^2")
 )
+
+# Exporting model
+saveRDS(model3, "models/valuation_model.rds")
